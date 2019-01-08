300c66ea3b05a69d73d8d0fb34ae7771d5cee529
lensonzhu <136453029@qq.com>
2019-01-09 00:22:51 +0800

    8d

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,8 +5,20 @@ from flask import render_template,redirect,request,url_for,flash
 from flask.ext.login import login_user
 from . import auth
 from .. models import User
-from . forms import LoginForm
+from . forms import LoginForm,RegistrationForm
 from flask.ext.login import logout_user,login_required
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        user=User(email=form.email.data,username=form.username.data,password=form.password.data)
+        db.session.add(user)
+        flash('You can now to login.')
+        return redirect(url_fot('auth.login'))
+    return render_template('auth/register.html',form=form)
+
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
--- app/models.py
+++ app/models.py
@@ -2,6 +2,10 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin
 from . import db,login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+from . import db
+
 
 class Role(db.Model):
     __tablename__='roles'
@@ -20,6 +24,24 @@ class User(UserMixin,db.Model):
 
     password_hash=db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default=False)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed=True
+        db.session.add(self)
+        return True
+
 
     def __repr__(self):
         return '<User %r>' % self.username
